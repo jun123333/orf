723e800e830996dd6306d0d979b58ef9891daa6f
jun123333 <rabbilution@gmail.com>
2021-11-03 20:53:20 +0900

    asdfdkj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
         <div class="container">
             <a href="javascript:void(0)" title="Logo" class="logo scroll">
                 <!--Logo Default-->
-                <img src="images/logo-white.png" alt="logo" class="logo-dark default">
+                <img src="images/logo-black.png" alt="logo" class="logo-dark default">
             </a>
 
             <!--Nav Links-->
@@ -114,44 +114,73 @@
 <!--Header end-->
 
 <!--slider-->
-<section id="home" class="p-0 single-slide fullscreen ">
+<section id="home" class="p-0 single-slide">
     <h2 class="d-none">hidden</h2>
     <div id="rev_slider_19_1_wrapper" class="rev_slider_wrapper fullscreen-container" data-alias="wexim_slider_01" data-source="gallery" style="background:transparent;padding:0px;">
         <!-- START REVOLUTION SLIDER 5.4.8.1 fullscreen mode -->
         <div id="rev_slider_19_1" class="rev_slider fullscreenbanner" style="display:none;" data-version="5.4.8.1">
-            <ul>
-                <!-- SLIDE  -->
-                <li data-index="rs-3" data-transition="crossfade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="default"  data-thumb="images/slide-img2.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
-
+            <ul>    <!-- SLIDE  -->
+                <li data-index="rs-94" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="300"  data-rotate="0"  data-saveperformance="off"  data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
                     <!-- MAIN IMAGE -->
-                    <img src="video/thumb.jpg"  alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="off" class="rev-slidebg" data-no-retina>
+                    <img src="images/bg-minimal.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="off" class="rev-slidebg" data-no-retina>
                     <!-- LAYERS -->
 
-                    <!-- Overlay -->
-                    <div class="bg-overlay opacity-8 z-index-1"></div>
+                    <!-- LAYER NR. 3 -->
+                    <div class="tp-caption   tp-resizeme"
+                         id="slide-94-layer-3"
+                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                         data-y="['middle','middle','middle','middle']" data-voffset="['-81','-81','-64','-48']"
+                         data-fontsize="['25','25','18','14']"
+                         data-lineheight="['20','20','18','14']"
+                         data-width="none"
+                         data-height="none"
+                         data-whitespace="nowrap"
 
-                    <div class="rs-background-video-layer"
-                         data-forcerewind="on"
-                         data-volume="mute"
-                         data-videowidth="100%"
-                         data-videoheight="100vh"
-                         data-videomp4="video/video.mp4"
-                         data-videopreload="auto"
-                         data-videoloop="loopandnoslidestop"
-                         data-forceCover="1"
-                         data-aspectratio="16:9"
-                         data-autoplay="true"
-                         data-autoplayonlyfirsttime="false"></div>
+                         data-type="text"
+                         data-responsive_offset="on"
 
+                         data-frames='[{"delay":1979.8828125,"speed":1500,"sfxcolor":"#ffffff","sfx_effect":"blockfromtop","frame":"0","from":"z:0;","to":"o:1;","ease":"Power4.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                         data-textAlign="['inherit','inherit','inherit','inherit']"
+                         data-paddingtop="[0,0,0,0]"
+                         data-paddingright="[0,0,0,0]"
+                         data-paddingbottom="[0,0,0,0]"
+                         data-paddingleft="[0,0,0,0]"
+
+                         style="z-index: 7; white-space: nowrap; font-size: 25px; line-height: 20px; font-weight: 300; color: #ffffff; letter-spacing: 0px;font-family:'BMJUA';">오산르네상스포럼. </div>
+
+                    <!-- LAYER NR. 4 -->
+                    <div class="tp-caption   tp-resizeme"
+                         id="slide-94-layer-4"
+                         data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']"
+                         data-y="['middle','middle','middle','middle']" data-voffset="['1','1','0','0']"
+                         data-fontsize="['100','100','70','50']"
+                         data-lineheight="['100','100','70','50']"
+                         data-width="['600','600','600','600']"
+                         data-height="none"
+                         data-whitespace="nowrap"
+
+                         data-type="text"
+                         data-responsive_offset="on"
+
+                         data-frames='[{"delay":2929.8828125,"speed":1500,"frame":"0","from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power3.easeInOut"}]'
+                         data-textAlign="['inherit','inherit','inherit','inherit']"
+                         data-paddingtop="[0,0,0,0]"
+                         data-paddingright="[0,0,0,0]"
+                         data-paddingbottom="[0,0,0,0]"
+                         data-paddingleft="[0,0,0,0]"
+
+                         style="z-index: 8; white-space: nowrap;font-weight: 600; color: #ffffff; letter-spacing: 0px;font-family:'BMJUA';"> <div class="text-center" id="js-rotating">환영합니다.</div>
+                    </div>
 
                   
-
                 </li>
-
-
             </ul>
-    </div><!-- END REVOLUTION SLIDER -->
     </div>
+    <!-- END REVOLUTION SLIDER -->
+    </div>
+
+  
+
 </section>
 <!--slider end-->
 
