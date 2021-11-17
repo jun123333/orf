bafede9d6fa2381718db615e8a3bd0b918de5e28
jun123333 <rabbilution@gmail.com>
2021-11-17 21:01:34 +0900

    sdfdsf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -894,44 +894,7 @@
 
 
 
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
+
 
 
 
@@ -940,7 +903,43 @@
 <section id="incorporation" class="page-content">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
+
+            <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        오산르네상스 포럼 정관
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/1.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/2.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/3.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/4.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/5.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/6.png" width="100%" alt="image">
+                </div>
+                <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
+                    <img src="images/7.png" width="100%" alt="image">
+                </div>
+      </div>
+    </div>
+  </div>
+ 
+</div>
+           {{--  <div class="col-sm-12">
             <div class="blog-detail-item row">
                 <div class="blog-detail-img col-sm-12 mb-5 hover-effect">
                     <img src="images/1.png" width="100%" alt="image">
@@ -968,7 +967,7 @@
                 
 
             </div>
-        </div>
+        </div> --}}
         </div>
 
         <!--Heading-->
